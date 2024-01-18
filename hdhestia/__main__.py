8d4f3ae903158f83081767e662b07fc8e0d694a8
Danilo Paes <danilo.paes@pm.me>
2024-01-18 14:30:01 +0100

    fix: import

--- hdhestia/__main__.py
+++ hdhestia/__main__.py
@@ -1,7 +1,7 @@
 import logging
 from argparse import ArgumentParser
-from hdhestia.arguments import args_list
-from hdhestia.argument_definer import ArgumentDefiner
+from .arguments import args_list
+from .argument_definer import ArgumentDefiner
 
 
 logging.basicConfig(
